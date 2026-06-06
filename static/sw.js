24d942c2fd38f7e51d1dbd8fa0163b8100c16f5c
kaego-ai <ryoarka@gmail.com>
2026-06-06 15:46:33 +0700

    Tambah Service Worker PWA

---
+++ static/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'kaego-v1';
+const urlsToCache = [
+    '/',
+    '/static/logo.png',
+    '/static/favicon.ico'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) return response;
+            return fetch(event.request);
+        })
+    );
+});
